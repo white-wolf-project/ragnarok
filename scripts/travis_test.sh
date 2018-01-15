57b6af6e07b0d691b45b02f27482c83113f0a202
Mathieu Hautebas <matteyeux@users.noreply.github.com>
2018-01-15 22:55:43 +0100

    Update travis_test.sh

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -4,9 +4,7 @@ sudo apt-get install -y make build-essential libssl-dev wget git python3  m4 aut
 
  git clone https://github.com/white-wolf-project/wireless-tools.git
  ls
- cd wireless-tools/wireless-tools
- sudo make install
- cd ..
+ cd wireless-tools/wireless-tools && ls && sudo make install
  sudo ldconfig
 
  make
