6e54857ed7aafbd215f55ef8dcbd18e6409884df
Mathieu Hautebas <matteyeux@users.noreply.github.com>
2018-01-15 22:47:55 +0100

    Fix travis script

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -3,8 +3,9 @@
 sudo apt-get install -y make build-essential libssl-dev wget git python3  m4 autoconf libtool autotools-dev libiw-dev libxml2-dev
 
  git clone https://github.com/white-wolf-project/wireless-tools.git
- sudo make -C wireless-tools/wireless-tools install
- 
+ cd wireless-tools/wireless-tools
+ sudo make install
+ cd ..
  sudo ldconfig
 
  make
