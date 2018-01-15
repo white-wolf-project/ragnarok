61b1fa3805cdacedf35c8098164fc577ee6e2d01
Mathieu Hautebas <matteyeux@users.noreply.github.com>
2018-01-15 22:51:51 +0100

    debugging travis

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -3,6 +3,7 @@
 sudo apt-get install -y make build-essential libssl-dev wget git python3  m4 autoconf libtool autotools-dev libiw-dev libxml2-dev
 
  git clone https://github.com/white-wolf-project/wireless-tools.git
+ ls
  cd wireless-tools/wireless-tools
  sudo make install
  cd ..
