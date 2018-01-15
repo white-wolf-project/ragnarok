30299bd2d6560f0c7ce56f665b9b64ca77a3fd65
Mathieu Hautebas <matteyeux@users.noreply.github.com>
2018-01-15 23:07:57 +0100

    trying to fix travis

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -4,7 +4,12 @@ sudo apt-get install -y make build-essential libssl-dev wget git python3  m4 aut
 
  git clone https://github.com/HewlettPackard/wireless-tools.git
  ls
- cd wireless-tools/wireless-tools && ls && sudo make install
+ cd wireless-tools
+ ls
+ cd wireless-tools
+ ls
+ sudo make install
+ 
  sudo ldconfig
 
  make
