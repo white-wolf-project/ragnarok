84cefd0c41b1d32fb57bb13e4c134a7d949b0e7c
mathieu <mathieu.hautebas@gmail.com>
2018-01-27 18:51:00 +0100

    call tcp server with parsed args in config/server.xml

--- src/server/main.c
+++ src/server/main.c
@@ -5,10 +5,14 @@
 #include <include/server.h>
 #include <include/server_tool.h>
 #include <include/common.h>
+#include <include/xml.h>
 
 int main(void){
 	signal(SIGINT, INThandler);
 	debug("starting server...\n");
-	tcp_server("12345");
+	// parse file to get iface (for sysnet) and port to run server
+	parse_config_file("config/server.xml");
+	fprintf(stdout, "interface : %s\nport : %s\n", iface, port);
+	tcp_server(port);
 	return 0;
 }
