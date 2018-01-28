a3ecedfb398a4e6cf1678da301c93e0e4d4644ee
mathieu <mathieu.hautebas@gmail.com>
2018-01-28 15:02:31 +0100

    add function to check if file exists

--- src/common.c
+++ src/common.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <Python.h>
+#include <stdbool.h>
 
 #include <include/common.h>
 
@@ -8,8 +9,13 @@
 static FILE* dbg = NULL;
 #endif
 
-void debug(const char* format, ...)
-{
+/*
+*	We call this function almost everywhere
+*	But it prints to stdout/stderr only
+*	when you build the project in DEBUG mode
+*	for the moment
+*/
+void debug(const char* format, ...) {
 	#ifdef DEBUG
 	va_list vargs;
 	va_start(vargs, format);
@@ -18,10 +24,29 @@ void debug(const char* format, ...)
 	#endif
 }
 
+/*
+*	This functions checks if a file exists
+*	returns TRUE if exists
+*	in the other case it returns FALSE.
+*	param is the name of the file
+*/
+bool file_exists(const char* file){
+	if(access(file, F_OK ) != -1) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/*
+*	Call a Python script with this function
+*	param is the python script you want to run
+*	We use Python3
+*/
 int run_python(const char *pyscript){
 	Py_Initialize();
 	FILE* file = fopen(pyscript, "r");
-	if (file == NULL)
+	if (!file_exists(pyscript))
 	{
 		fprintf(stderr, "%s: %s\n", pyscript, strerror(errno));
 		return -2;
