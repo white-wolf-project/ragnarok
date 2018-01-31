0b3e3aa024639886dba783c9a2a8efe453cb2783
matteyeux <mathieu.hautebas@gmail.com>
2018-01-31 07:59:01 -0800

    sysnet will build

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -13,22 +13,22 @@
 #
 
 # update repos before installing
-sudo apt-get update && sudo apt-get upgrade -y
+#sudo apt-get update && sudo apt-get upgrade -y
 
 # install build tools 
-sudo apt-get install -y make build-essential libssl-dev libreadline-dev libsqlite3-dev wget git python3 \
-libnl-3-dev apache2 nmap m4 autoconf libtool autotools-dev libiw-dev libxml2-dev vim zsh htop python3.5-dev
+#sudo apt-get install -y make build-essential libssl-dev libreadline-dev libsqlite3-dev wget git python3 \
+#libnl-3-dev apache2 nmap m4 autoconf libtool autotools-dev libiw-dev libxml2-dev vim zsh htop python3.5-dev
 
-if [[ $(arch) != "armv71" && $(arch) != "aarch64" ]];then
-	sudo apt-get install -y gcc-arm-linux-gnueabihf
-fi
+#if [[ $(arch) != "armv71" && $(arch) != "aarch64" ]];then
+#	sudo apt-get install -y gcc-arm-linux-gnueabihf
+#fi
 
 # init and clone git modules
 git submodule update --init
 
 # install git depends
-git clone https://github.com/matteyeux/sysnet; cd sysnet
-./scripts/install.sh
+git clone https://github.com/matteyeux/sysnet; cd sysnet/scripts
+./install.sh
 sudo make install
 cd ..
 
