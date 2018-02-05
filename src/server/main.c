b4cdf8e568791813f203d4f800fdc6e4050bbaf7
matteyeux <mathieu.hautebas@gmail.com>
2018-02-05 14:14:30 -0800

    first commit with build type

--- src/server/main.c
+++ src/server/main.c
@@ -8,12 +8,25 @@
 #include <include/xml.h>
 #include <include/network.h>
 
+// Not sure it is the best way to do what I want to do.
+// TODO : find another way if the user wants to modify the path
+// to the XML file (eg : with args)
+#ifdef RELEASE
+	#define CONFIG_PATH "/etc/ragnarok/server.xml"
+#elif DEVELOPMENT
+	#define CONFIG_PATH "config/server.xml"
+#else // default is DEVELOPMENT
+	#define CONFIG_PATH "config/server.xml"
+#endif
+
 int main(void){
 	signal(SIGINT, INThandler);
 	debug("starting server...\n");
 
 	// parse file to get iface (for sysnet) and port to run server
-	parse_config_file("config/server.xml");
+	if (parse_config_file(CONFIG_PATH) != 0)
+		return -1;
+
 	printf("\t");
 
 	// call network_info from sysnet to get IP of the server
