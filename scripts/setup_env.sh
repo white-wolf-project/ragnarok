e9d994527a083807bd0347864da6c0e606499706
mathieu <mathieu.hautebas@gmail.com>
2018-04-17 22:58:14 +0200

    add libmysql++-dev installation in the setup script

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -34,7 +34,7 @@ function install_libcpuid(){
 sudo apt-get update && sudo apt-get upgrade -y
 
 # install build tools 
-sudo apt-get install -y make build-essential libssl-dev libreadline-dev libsqlite3-dev wget git python3 python3-pip \
+sudo apt-get install -y make build-essential libssl-dev libreadline-dev libsqlite3-dev libmysql++-dev wget git python3 python3-pip \
 libnl-3-dev apache2 nmap m4 autoconf libtool autotools-dev libiw-dev libxml2-dev vim zsh htop python3.5-dev
 
 if [[ $(arch) != "armv71" && $(arch) != "aarch64" ]];then
