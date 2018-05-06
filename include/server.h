81001ac4e2953bf0d3b7ffe5dea33d84f9d189c2
mathieu <mathieu.hautebas@gmail.com>
2018-05-06 14:44:51 +0200

    add pidfile as a param, and change function prototype

--- include/server.h
+++ include/server.h
@@ -8,7 +8,7 @@ char *iface, *port;
 void INThandler(int sig);
 void manage_co(int sock, int counter);
 
-int init_srv_daemon(void);
+int init_srv_daemon(const char *pidfile);
 int tcp_server(const char* service_port);
 
 bool check4db(const char *db_name);
