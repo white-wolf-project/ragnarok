abf5da480eea56bd814cab394a72756a10f8fb08
mathieu <mathieu.hautebas@gmail.com>
2018-05-28 10:18:43 -0700

    add new test file, to remove quickly database tables

---
+++ test/remove_tables.sh
@@ -0,0 +1,3 @@
+mysql --user="root" -p"root" --database="ragnarok_bdd" --execute="delete from Info_AP;"
+mysql --user="root" -p"root" --database="ragnarok_bdd" --execute="delete from Encryption;"
+mysql --user="root" -p"root" --database="ragnarok_bdd" --execute="delete from Device_info;"
